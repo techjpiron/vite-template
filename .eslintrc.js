6b045fdc6407bc1ce5647ca4654a463615d14331
Julien Piron <tech@jpiron.eu>
2022-02-09 15:46:08 +0100

    chore: install husky, commitlint, lint-staged, eslint, and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ .lintstagedrc.js
@@ -0,0 +1,5 @@
+/* eslint-env node */
+module.exports = {
+  "*.{js,ts,jsx,tsx}": ["eslint --max-warnings=0", "prettier --write"],
+  "*.{html,md,css,json}": ["prettier --write"],
+};
