70094f9b7d14e045452b5f8ef4663f15ee8db831
Julien Piron <tech@jpiron.eu>
2022-04-03 12:52:07 +0200

    chore: upgrade dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./tailwind.css"
+import { createRoot } from "react-dom/client"
+import { StrictMode } from "react"
 import App from "./App"
+import "./tailwind.css"
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById("root")
+
+if (!container) {
+  throw new Error("Couldn't find #root")
+}
+
+const root = createRoot(container)
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 )
